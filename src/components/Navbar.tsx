54e1a94417c7bde8532d32f9024bce6b5eb0321a
admin1 <admin1@N001039.STERMAX.AD>
2025-11-14 11:37:10 -0300

    verifica so se ja tiver o cookie setado

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,6 +32,11 @@ export default function Navbar() {
         }
     }
     useEffect(() => {
+        if (!document.cookie.includes("token=")) {
+            setLoading(false);
+            return;
+        }
+
         fetchUser();
     }, []);
 
